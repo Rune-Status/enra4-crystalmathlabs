0a606592cb8f8b3e8fa485f2bae665edadaeb39a
nikersify <nikersifyyt@gmail.com>
2017-02-23 15:08:24 +0100

    tests: add test 'framework' & stub http requests

---
+++ test/api.js
@@ -0,0 +1,13 @@
+const fs = require('fs')
+
+if (!fs.existsSync(__dirname + '/cache/cache.json')) {
+	console.log('do \'npm run cache-gen\' before running the tests!')
+	process.exit(1)
+}
+
+const cache = require('./cache/cache.json')
+
+module.exports = (url, cb) => {
+	const r = cache[url]
+	cb(r.error, null, r.body)
+}
