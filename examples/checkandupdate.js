db86e292f527dce6535fd71946e76859239caf22
enra <cudcsb@gmail.com>
2017-02-23 01:22:38 +0100

    started examples

---
+++ examples/checkandupdate.js
@@ -0,0 +1,19 @@
+// checks to see if a user has been checked in the last day
+// and if not, update user and get the stats gained during last day
+
+const CML = require('../lib/crystalmethlabs')
+const meth = new CML()
+
+meth.lastcheck('lynx titan', (err, sec) => {
+	let day = 24 * 360 // a day in seconds
+
+	if(sec > day) {
+		meth.update('lynx titan', (err) => {
+			meth.track('lynx titan', day, (err, stats) => {
+				console.log(stats)
+			})
+		})
+	} else {
+		console.log('lynx titan has recently been updated.')
+	}
+})
